80e96ee032f5563df534ab5c105cd52937d5127e
Yang Nan <nanyang@kth.se>
2021-01-08 00:05:43 +0200

    1.11

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
+import fs from 'fs';
 
 const app = express();
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
 const PORT = 3001;
-
+const PATH = '/tmp/kube';
 let counter = 0;
 
 app.get('/', (_req, res) => {
-  res.send(`pong ${counter}`);
   counter++;
-})
+  res.send(`pong ${counter}`);
+  fs.writeFileSync(`${PATH}/ping-pong.txt`, `Ping / Pong: ${counter}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
-})
+});
