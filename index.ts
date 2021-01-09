09d1693fb47e2915c0eb77a6ff282d07fa30f3f5
Yang Nan <nanyang@kth.se>
2021-01-10 01:48:30 +0200

    2.01

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
-import fs from 'fs';
+// import fs from 'fs';
 
 const app = express();
 
 app.use(morgan('tiny'));
 
 const PORT = 3002;
-const PATH = './files';
+// const PATH = './files';
 let counter = 0;
 
-const writeFile = () => fs.writeFileSync(`${PATH}/ping-pong.txt`, `Ping / Pong: ${counter}`);
-writeFile();
+// const writeFile = () => fs.writeFileSync(`${PATH}/ping-pong.txt`, `Ping / Pong: ${counter}`);
+// writeFile();
 
 app.get('/', (_req, res) => {
   counter++;
   res.send(`pong ${counter}`);
-  writeFile();
+  // writeFile();
 });
 
 app.get('/inquire', (_req, res) => {
