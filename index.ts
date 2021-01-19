739f25c4ee3ab7f3c06f2d9b6dbf0e6a2cd72962
Yang Nan <nanyang@kth.se>
2021-01-20 00:26:01 +0200

    2.07

--- index.ts
+++ index.ts
@@ -22,16 +22,19 @@ app.get('/', async (_req, res) => {
   } catch (err) {
     console.error(err);
   }
-  // writeFile();
 });
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.get('/inquire', async (_req, res) => {
-  const response = await pool.query('SELECT counter FROM pingpong WHERE id = 1');
-  const rows = parseRows(response.rows);
-  if (!rows[0]) throw new Error('rows is empty');
-    const counter = rows[0].counter;
-  res.send(`Ping / Pong: ${counter}`);
+  try {
+    const response = await pool.query('SELECT counter FROM pingpong WHERE id = 1');
+    const rows = parseRows(response.rows);
+    if (!rows[0]) throw new Error('rows is empty');
+      const counter = rows[0].counter;
+    res.send(`Ping / Pong: ${counter}`);
+  } catch (err) {
+    console.error(err);
+  }  
 });
 
 app.listen(PORT, () => {
